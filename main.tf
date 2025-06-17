68d16aae0224d7cd2f5c36dbd40405d102e11af2
Nikita-jeff-app <149382044+Nikita-jeff-app@users.noreply.github.com>
2025-06-17 08:47:30 +0300

    updates

--- main.tf
+++ main.tf
@@ -20,10 +20,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "my_dummy_bucket" {
-  bucket = "dummy-bucket-nikita"
-}
-
 
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
