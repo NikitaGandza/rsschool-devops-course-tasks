eeb606bb781638d6869bddbbfbfa433953a63090
Nikita-jeff-app <149382044+Nikita-jeff-app@users.noreply.github.com>
2025-06-17 08:49:12 +0300

    updates

--- main.tf
+++ main.tf
@@ -19,16 +19,3 @@ terraform {
     dynamodb_table = "terrform-state"
   }
 }
-
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-}
