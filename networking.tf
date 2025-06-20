56bf996548b481c3e1060dd04673ba8ecefc60b7
NikitaGandza <gannikita@gmail.com>
2025-06-20 21:20:36 +0300

    task_2 init

---
+++ networking.tf
@@ -0,0 +1,92 @@
+
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+
+ tags = {
+   Name = "rsschool-vpc"
+ }
+}
+
+resource "aws_subnet" "public_subnet_eur_north_1a" {
+  count             = 1
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_a
+  availability_zone = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public subnet eu-north-1a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_eur_north_1a" {
+  count             = 1
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr_a
+  availability_zone = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private subnet eu-north-1a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_eur_north_1b" {
+  count             = 1
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_b
+  availability_zone = "eu-north-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public subnet eu-north-1b"
+  }
+}
+
+resource "aws_subnet" "private_subnet_eur_north_1b" {
+  count             = 1
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr_b
+  availability_zone = "eu-north-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private subnet eu-north-1b"
+  }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name    = "public_route_table"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name    = "private_route_table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc_a" {
+  subnet_id      = aws_subnet.public_subnet_eur_north_1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_assoc_b" {
+  subnet_id      = aws_subnet.public_subnet_eur_north_1b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_assoc_a" {
+  subnet_id      = aws_subnet.private_subnet_eur_north_1a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_assoc_b" {
+  subnet_id      = aws_subnet.private_subnet_eur_north_1b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
+variable "public_subnet_cidr_a" {
+ type        = string
+ description = "Public Subnet CIDR value eu-north-1a"
+ default     = "10.0.1.0/24"
+}
 
+variable "private_subnet_cidr_a" {
+ type        = string
+ description = "Private Subnet CIDR value eu-north-1a"
+ default     = "10.0.2.0/24"
+}
+
+variable "public_subnet_cidr_b" {
+ type        = string
+ description = "Public Subnet CIDR value eu-north-1b"
+ default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_cidr_b" {
+ type        = string
+ description = "Private Subnet CIDR value eu-north-1b"
+ default     = "10.0.4.0/24"
+}
