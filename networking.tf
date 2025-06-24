2efeb44943d1f07500d92ca7fa7d4b4e4d42b2cd
NikitaGandza <gannikita@gmail.com>
2025-06-24 18:09:38 +0300

    task_2

--- networking.tf
+++ networking.tf
@@ -1,122 +1,113 @@
-
-
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
-
- tags = {
-   Name = "rsschool-vpc"
- }
-}
-
-resource "aws_subnet" "public_subnet_eur_north_1a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_a
-  availability_zone = "eu-north-1a"
-  map_public_ip_on_launch = true
-
+# VPC
+resource "aws_vpc" "terraform-lab-vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "Public subnet eu-north-1a"
+    Name = "terraform-lab-vpc"
   }
 }
 
-resource "aws_subnet" "private_subnet_eur_north_1a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr_a
-  availability_zone = "eu-north-1a"
-  map_public_ip_on_launch = true
-
+resource "aws_subnet" "public-subnet-1" { # NAT will be here
   tags = {
-    Name = "Private subnet eu-north-1a"
+    Name = "public-terraform-lab-subnet-1"
   }
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.terraform-lab-vpc.id
+  availability_zone = var.availability_zones[0]
 }
-
-resource "aws_subnet" "public_subnet_eur_north_1b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr_b
-  availability_zone = "eu-north-1b"
-  map_public_ip_on_launch = true
-
+resource "aws_subnet" "public-subnet-2" {
   tags = {
-    Name = "Public subnet eu-north-1b"
+    Name = "public-terraform-lab-subnet-2"
   }
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.terraform-lab-vpc.id
+  availability_zone = var.availability_zones[1]
 }
 
-resource "aws_subnet" "private_subnet_eur_north_1b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr_b
-  availability_zone = "eu-north-1b"
-  map_public_ip_on_launch = true
-
+# Private subnets
+resource "aws_subnet" "private-subnet-1" {
   tags = {
-    Name = "Private subnet eu-north-1b"
+    Name = "private-terraform-lab-subnet-1"
   }
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.terraform-lab-vpc.id
+  availability_zone = var.availability_zones[0]
 }
-
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "private-subnet-2" {
   tags = {
-    Name    = "public_route_table"
+    Name = "private-terraform-lab-subnet-2"
   }
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.terraform-lab-vpc.id
+  availability_zone = var.availability_zones[1]
 }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "terraform-lab-igw" {
   tags = {
-    Name    = "private_route_table"
+    Name = "terraform-lab-igw"
   }
+  vpc_id = aws_vpc.terraform-lab-vpc.id
 }
 
-resource "aws_route_table_association" "public_assoc_a" {
-  subnet_id      = aws_subnet.public_subnet_eur_north_1a.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "public_assoc_b" {
-  subnet_id      = aws_subnet.public_subnet_eur_north_1b.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "private_assoc_a" {
-  subnet_id      = aws_subnet.private_subnet_eur_north_1a.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_route_table_association" "private_assoc_b" {
-  subnet_id      = aws_subnet.private_subnet_eur_north_1b.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_internet_gateway" "rsschool_internet_gateway" {
-  tags = {
-    Name = "rsschool_internet_gateway"
-  }
-  vpc_id = aws_vpc.main.id
-}
-
+# NAT Gateway for the public subnet
 resource "aws_eip" "nat_gateway" {
   associate_with_private_ip = "10.0.0.5"
-  depends_on                = [aws_internet_gateway.rsschool_internet_gateway]
+  depends_on                = [aws_internet_gateway.terraform-lab-igw]
 }
 resource "aws_nat_gateway" "terraform-lab-ngw" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id     = aws_subnet.public_subnet_eur_north_1a.id
+  subnet_id     = aws_subnet.public-subnet-1.id
 
   tags = {
-    Name = "rsschool_nat_gateway"
+    Name = "terraform-lab-ngw"
   }
   depends_on = [aws_eip.nat_gateway]
 }
 
+# Route tables for the subnets
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.terraform-lab-vpc.id
+  tags = {
+    Name = "public-terraform-lab-route-table"
+  }
+}
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.terraform-lab-vpc.id
+  tags = {
+    Name = "private-terraform-lab-route-table"
+  }
+}
 
-resource "aws_route" "public_internet_igw_route" {
-  route_table_id = aws_route_table.public_route_table.id
-  gateway_id = aws_internet_gateway.rsschool_internet_gateway.id
+# Route the public subnet traffic through the Internet Gateway
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.terraform-lab-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route" "nat_gateway_route" {
-  route_table_id = aws_route_table.private_route_table.id
-  gateway_id = aws_nat_gateway.terraform-lab-ngw.id
+# Route NAT Gateway
+resource "aws_route" "nat-ngw-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.terraform-lab-ngw.id
   destination_cidr_block = "0.0.0.0/0"
+}
+
+# Associate the newly created route tables to the subnets
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
 }
